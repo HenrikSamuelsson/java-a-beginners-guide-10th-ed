ba3fc31cf5cd60157860380d28c5ab02847b6c1e
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-05-21 06:43:50 +0200

    Auto formatted code.

--- chapter-01/try-this-01-01/GalToLit.java
+++ chapter-01/try-this-01-01/GalToLit.java
@@ -6,12 +6,12 @@
 
 public class GalToLit {
     public static void main(String[] args) {
-        double gallons;    // Holds number of gallons.
-        double litres;     // Holds number of litres.
+        double gallons; // Holds number of gallons.
+        double litres; // Holds number of litres.
 
-        gallons = 10;    // Hard coded number of gallons.
-        
-        litres = gallons * 3.7854;    // Convert gallons to litres.
-        System.out.println(gallons + " gallons is " + litres + " litres."); 
+        gallons = 10; // Hard coded number of gallons.
+
+        litres = gallons * 3.7854; // Convert gallons to litres.
+        System.out.println(gallons + " gallons is " + litres + " litres.");
     }
 }
