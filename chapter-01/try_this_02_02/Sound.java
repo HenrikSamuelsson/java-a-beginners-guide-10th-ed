10ec6a6aa29394b5211a732091441487ad594290
Henrik Samuelsson <henrik.samuelsson2@saabgroup.com>
2024-05-22 06:50:16 +0200

    Add program that calculates distance to lightning.

--- chapter-01/try_this_02_02/Sound.java
+++ chapter-01/try_this_02_02/Sound.java
@@ -4,3 +4,21 @@
  * Compute the distance to a lightning strike whose sound takes 7.2 seconds to
  * reach you.
  */
+class Sound {
+    public static void main(String[] args) {
+        double time_s = 7.2; // Time from lighting strike until sound is heard.
+
+        double speedOfSound_ms = 343; // Speed of sound in meters per second.
+        double speedOfSound_fs = 1125; // Speed of sound in foot per second.
+
+        // Calculate distance to lightning strike in meters.
+        double distance_m = time_s * speedOfSound_ms;
+
+        // Calculate distance to lightning strike in feet.
+        double distance_f = time_s * speedOfSound_fs;
+
+        // Present result of the calculations.
+        System.out.println("The lightning is " + distance_m + " meters away ("
+                + distance_f + " feet away).");
+    }
+}
